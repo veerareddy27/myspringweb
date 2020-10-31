5b662d2ac886f90b6cd00cfb27273686c523bef1
Veera Reddy <veerareddy27@users.noreply.github.com>
2020-10-31 10:14:55 -0400

    myspringweb

---
+++ src/main/java/com/thd/myspringweb/MySpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.thd.myspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thd/myspringweb/MySpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thd.myspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
